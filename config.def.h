8823ab27ec7705072e42580f853efc0f4950d200
Igor Boyarshin <igor.boyarshin@gmail.com>
2020-05-28 16:35:32 +0300

    Applied patch dwm-statuscolors-20181008-b69c870.diff with a personal fix

--- config.def.h
+++ config.def.h
@@ -9,7 +9,7 @@ static const char *fonts[]          = { "SauceCodePro Nerd Font Mono:size=11:ant
 static const char dmenufont[]       = "SauceCodePro Nerd Font Mono:size=11:antialias=true:style=Meduim:minspace=False:hinting=true";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
+static const char col_gray3[]       = "#0bbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
 static const unsigned int baralpha = 0xd0;
@@ -18,11 +18,13 @@ static const char *colors[][3]      = {
     /*               fg         bg         border   */
     [SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
     [SchemeSel]  = { col_gray4, "#0033f5", "#ff3335" },
+    [SchemeTest]  = { "#00ff00", "#f000ff", "#ff0000" },
 };
 static const unsigned int alphas[][3]      = {
     /*               fg      bg        border     */
     [SchemeNorm] = { OPAQUE, baralpha, borderalpha },
     [SchemeSel]  = { OPAQUE, baralpha, borderalpha },
+    [SchemeTest]  = { OPAQUE, baralpha, borderalpha },
 };
 
 /* tagging */
